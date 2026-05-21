85722d78ec6cc6cb82b5380d368c4adc7fb0524e
sirajDev404 <sirajkhan07536@gmail.com>
2026-05-21 14:15:37 +0700

    tweaks

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./config/global.jsx"
 import { BrowserRouter } from 'react-router-dom'
-import AppProvider from './Context/AppProvider.jsx'
+import AppProvider from './Context/AppProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
